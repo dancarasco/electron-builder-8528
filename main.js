20759ed2cca9eff4e4fef2d610211c6789369ec8
Dan Carasco <daniel.carasco@g2-travel.com>
2024-09-24 16:25:32 +0100

    App

---
+++ main.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const sqlite3 = require('sqlite3')
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+
+    // SQLite3 database setup
+    const db = new sqlite3.Database(':memory:');
+
+    db.serialize(() => {
+        db.run("CREATE TABLE lorem (info TEXT)");
+
+        const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        for (let i = 0; i < 10; i++) {
+            stmt.run("Ipsum " + i);
+        }
+        stmt.finalize();
+
+        db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+            console.log(row.id + ": " + row.info);
+        });
+    });
+
+    db.close();
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
